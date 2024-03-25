957caafe8ce7b52f431ea9bd509b10929a5c4d2e
TheMadDrunk <iophamza2@gmail.com>
2024-03-25 04:07:45 +0100

    converts circles to paths and adds propreties

--- index.js
+++ index.js
@@ -1,114 +1,102 @@
 const { geoFromSVGXML } = require('svg2geojson');
 const fs = require('fs');
-const { DOMParser,XMLSerializer } = require('xmldom-qsa');
+const { DOMParser, XMLSerializer } = require('xmldom-qsa');
+const { features } = require('process');
 
-
-
-
-// var stringSVG = fs.readFileSync('svgsrc.svg') + "";
-// const parser = new DOMParser();
-// const doc = parser.parseFromString(stringSVG, 'image/svg+xml');
-
-
-
-// const seatGroups = doc.querySelectorAll('g.SEAT')
-
-// const newDoc = new DOMParser().parseFromString(`<svg xmlns="http://www.w3.org/2000/svg"><MetaInfo xmlns="http://www.prognoz.ru"><Geo>
-// <GeoItem X="-595.30" Y="-142.88" Latitude="37.375593" Longitude="-121.977795"/>
-// <GeoItem X="1388.66" Y=" 622.34" Latitude="37.369930" Longitude="-121.959404"/>
-// </Geo></MetaInfo></svg>`, 'image/svg+xml');
-
-
-// seatGroups.forEach(group => {
-//     const clonedGroup = group.cloneNode(true); 
-//     newDoc.documentElement.appendChild(clonedGroup);
-// });
-
-// const newSVGString = new XMLSerializer().serializeToString(newDoc);
-// console.log(newSVGString)
-// geoFromSVGXML( newSVGString, layer => {
-//     //console.log(layer)
-//     let json = JSON.stringify(layer); // Turn JS object into JSON string
-//      fs.writeFileSync('extracted_seats.geojson', json, 'utf-8');
-//      console.log('Extracted seats saved to extracted_seats.svg');
-// },{tolerance:0.001} );
-
-
-
-
-
-
-
-
-function circleToPath(circleString) {
-    // Parse the circle string to extract attributes
-    const parser = new DOMParser();
-    const circleDOM = parser.parseFromString(circleString, 'image/svg+xml').documentElement;
-    const cx = parseFloat(circleDOM.getAttribute('cx'));
-    const cy = parseFloat(circleDOM.getAttribute('cy'));
-    const r = parseFloat(circleDOM.getAttribute('r'));
+function circleToPath(circle) {
+    const cx = parseFloat(circle.getAttribute('cx'));
+    const cy = parseFloat(circle.getAttribute('cy'));
+    const r = parseFloat(circle.getAttribute('r'));
 
     // Constants
     const numSegments = 64; // Number of line segments for precision
     const angleIncrement = (Math.PI * 2) / numSegments;
 
     // Generate path data
-    const pathData = [];
+    let pathData = "";
     const startX = cx + r;
     const startY = cy;
-    pathData.push(`M ${startX},${startY}`);
+    pathData += `M ${startX} ${startY} `;
 
     // Iterate over each segment to generate the path
     for (let i = 1; i <= numSegments; i++) {
         const angle = angleIncrement * i;
         const x = cx + Math.cos(angle) * r;
         const y = cy + Math.sin(angle) * r;
-        pathData.push(`L ${x},${y}`);
+        pathData += `L ${x.toFixed(6)} ${y.toFixed(6)} `;
     }
 
     // Close the path
-    pathData.push('Z');
+    pathData += "Z "
 
-    // Combine path segments into a single string
-    pathData.join(' ');
+    const pathElement = `<path d="${pathData}" />`;
 
-     // Construct the <path> element string
-     const pathElement = `<path d="${pathData}" />`;
+    const parser = new DOMParser();
+    const path = parser.parseFromString(pathElement, 'image/svg+xml').documentElement;
 
-     // Return the <path> element string
-     return pathElement;
+    return path;
 }
 
-const circleString = '<circle cx="50" cy="50" r="40" />';
-const pathString = circleToPath(circleString);
-console.log(pathString);
+function convertCirclesIntoPaths(doc) {
+    const circleList = doc.querySelectorAll('circle')
+    if(circleList.length == 0)
+        return;
+    circleList.forEach(circle => {
 
+        path = circleToPath(circle)
 
+        circle.parentNode.replaceChild(path, circle);
+    });
+}
 
+// 1 -opens the file
+// 2 - replace circles with paths
+// 3 - convert by layers to geojson
+var stringSVG = fs.readFileSync('stade test (1).svg') + "";
+const parser = new DOMParser();
+const doc = parser.parseFromString(stringSVG, 'image/svg+xml');
+console.log('1 - SVG loaded.');
 
+convertCirclesIntoPaths(doc)
+console.log('2 - Converted circles into paths');
 
+const dataobjstring = fs.readFileSync('props.json', 'utf8');
+const dataobj = JSON.parse(dataobjstring);
+let geojsonResult = {
+    "type": "FeatureCollection",
+    "creator": "s2gprops",
+    "features": []
+};
 
+dataobj.classProps.forEach(feat => {
+    const docGeoPos = new DOMParser().parseFromString(`<svg xmlns="http://www.w3.org/2000/svg"><MetaInfo xmlns="http://www.prognoz.ru"><Geo>
+    <GeoItem X="-595.30" Y="-142.88" Latitude="37.375593" Longitude="-121.977795"/>
+    <GeoItem X="1388.66" Y=" 622.34" Latitude="37.369930" Longitude="-121.959404"/>
+    </Geo></MetaInfo></svg>`, 'image/svg+xml');
 
+    const svglayer = doc.querySelectorAll("g."+feat.className)
 
+    if(svglayer.length == 0){
+        console.log("No group '"+feat.className+"' found")
+        return;
+    }
 
+    svglayer.forEach(group => {
+        const clonedGroup = group.cloneNode(true);
+        docGeoPos.documentElement.appendChild(clonedGroup);
+    });
 
+    const newSVGString = new XMLSerializer().serializeToString(docGeoPos);
+    geoFromSVGXML( newSVGString, layer => {
+        layer.features.forEach(feature => {
+            feature.properties = feat.propreties
+        })
+        
+        geojsonResult.features = geojsonResult.features.concat(layer.features)
 
+        console.log("Added properties to "+feat.className)
+    } );
+})
 
-
-
-
-
-// fs.writeFileSync('extracted_seats.svg', newSVGString, 'utf-8');
-
-// console.log('Extracted seats saved to extracted_seats.svg');
-
-// var x = 1;
-// geoFromSVGFile( 'svgsrc.svg', layers => {
-//     layers.forEach( layer => {
-//         console.log("layer:",x)
-//         console.log(`Layer Named: "${layer.name}"`);
-//         x++
-//     });
-// }, {layers:true} ); 
-
+fs.writeFileSync('gjResult.geojson',JSON.stringify(geojsonResult));
 
